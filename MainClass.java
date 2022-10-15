6cf6aaed3f02957097528a346e5234ecc9e15255
kothale19 <109896578+kothale19@users.noreply.github.com>
2022-10-15 23:36:21 +0530

    Add files via upload
    "Added: Generics Problem"

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+package com.blz.generics;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MaxValueExtends.java
@@ -0,0 +1,10 @@
+package com.blz.generics;
+
+public class MaxValueExtends {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
